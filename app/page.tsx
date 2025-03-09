b0a861c42ea91b1a30ead92adcd0f56ac3a0ce36
Manuel <juanzatch17@hotmail.com>
2025-03-09 12:23:15 -0400

    Created initial template for page

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <section className='py-20'>
+      <div className='container'>
+        <h1 className='text-3xl font-bold'>Started</h1>
+      </div>
+    </section>
+  )
+}
