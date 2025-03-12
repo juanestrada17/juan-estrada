7bc28eb945151eb983571c1a21c30fc10dff9602
Manuel <juanzatch17@hotmail.com>
2025-03-11 21:20:25 -0400

    Added new font and font size

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import { Roboto } from 'next/font/google'
+
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const roboto = Roboto({
+  weight: '400',
   subsets: ['latin']
 })
 
@@ -27,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang='en' className='hover:dark:text-white"'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} mx-auto flex min-h-screen flex-col bg-white px-4 sm:px-8 md:px-16 lg:px-32 xl:px-48 2xl:mx-[250px]`}
+        className={`${roboto.className} mx-auto flex min-h-screen flex-col bg-white px-4 text-[18px] sm:px-8 md:px-16 lg:px-32 xl:px-48 2xl:mx-[250px]`}
       >
         <Navbar />
         <main className='flex-1'>{children} </main>
